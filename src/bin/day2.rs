870a86394fb079865be705f70f158b32aedc2a1e
jasilven <jasilven@gmail.com>
2020-05-15 19:14:26 +0300

    day2

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -41,18 +41,33 @@ fn parse_input(fname: &str) -> Result<Vec<isize>> {
     }
 }
 
-fn solve1(mut input: Vec<isize>) -> Result<isize> {
-    input[1] = 12;
-    input[2] = 2;
+fn solve1(mut input: Vec<isize>, pos1: Option<isize>, pos2: Option<isize>) -> Result<isize> {
+    input[1] = pos1.unwrap_or(input[1]);
+    input[2] = pos2.unwrap_or(input[2]);
+
     let mut cpu = Cpu::new(&input);
     cpu.execute()?;
 
     Ok(cpu.prog[0])
 }
 
+fn solve2(input: Vec<isize>) -> Result<isize> {
+    for noun in 0..100 {
+        for verb in 0..100 {
+            let prog = input.to_vec();
+            match solve1(prog, Some(noun), Some(verb)) {
+                Ok(19690720) => return Ok(100 * noun + verb),
+                _ => (),
+            }
+        }
+    }
+    anyhow::bail!("unable to find solution!")
+}
+
 fn main() -> Result<()> {
     let input = parse_input("resources/day2-input.txt")?;
-    println!("part 1: {}", solve1(input)?);
+    println!("part 1: {}", solve1(input.clone(), Some(12), Some(2))?);
+    println!("part 1: {}", solve2(input)?);
     Ok(())
 }
 
@@ -63,6 +78,6 @@ mod tests {
     #[test]
     fn test_execute() {
         let input = parse_input("resources/day2-test.txt").unwrap();
-        assert_eq!(3500, solve1(input).unwrap());
+        assert_eq!(3500, solve1(input, None, None).unwrap());
     }
 }
