ea31d0c4720dc9ac6d0b92b2f2d4fd3ba18f6028
jasilven <jasilven@gmail.com>
2020-05-22 17:20:21 +0300

    day4, part2

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use std::collections::HashSet;
 
 fn is_valid(num: usize) -> bool {
     let mut same_adjacents = false;
@@ -14,7 +15,24 @@ fn is_valid(num: usize) -> bool {
         }
     });
 
-    same_adjacents && inc_order
+    same_adjacents && inc_order && true
+}
+
+fn is_valid2(num: usize) -> bool {
+    let chs: Vec<char> = num.to_string().chars().collect();
+
+    let pairs = chs
+        .windows(2)
+        .filter(|chs| chs[0] == chs[1])
+        .map(|chs| chs[0])
+        .collect::<HashSet<char>>();
+    let triples = chs
+        .windows(3)
+        .filter(|chs| (chs[0] == chs[1]) & (chs[0] == chs[2]))
+        .map(|chs| chs[0])
+        .collect::<HashSet<char>>();
+
+    pairs.difference(&triples).count() != 0
 }
 
 fn solve1(start: usize, end: usize) -> Result<Vec<usize>> {
@@ -28,9 +46,17 @@ fn solve1(start: usize, end: usize) -> Result<Vec<usize>> {
     Ok(nums)
 }
 
+fn solve2(start: usize, end: usize) -> Result<Vec<usize>> {
+    let nums = solve1(start, end)?;
+    let result = nums.iter().filter(|n| is_valid2(**n)).cloned().collect();
+    Ok(result)
+}
+
 fn main() -> Result<()> {
     let part1 = solve1(353096, 843212)?;
+    let part2 = solve2(353096, 843212)?;
     println!("part 1: {}", part1.len());
+    println!("part 2: {}", part2.len());
     Ok(())
 }
 
@@ -39,9 +65,16 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_part1() {
+    fn test_valid() {
         assert_eq!(true, is_valid(111111));
         assert_eq!(false, is_valid(223450));
         assert_eq!(false, is_valid(123789));
+    }
+
+    #[test]
+    fn test_valid2() {
+        assert_eq!(true, is_valid2(112233));
+        assert_eq!(false, is_valid2(123444));
+        assert_eq!(true, is_valid2(111122));
     }
 }
