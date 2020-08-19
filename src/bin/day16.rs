f5a5a2df9639d7c9479fdfeaeb47c49000db40f9
jasilven <jasilven@gmail.com>
2020-08-19 07:20:23 +0300

    day 16

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use rayon::prelude::*;
 
 fn parse_input(path: &str) -> Result<Vec<isize>> {
     let mut result = vec![];
@@ -15,40 +16,19 @@ fn parse_input(path: &str) -> Result<Vec<isize>> {
     Ok(result)
 }
 
-fn expand_pattern(index: usize, pattern: &[isize]) -> Vec<isize> {
-    let mut result = vec![];
-    for digit in pattern.iter() {
-        for i in 0..index {
-            result.push(*digit);
-        }
-    }
-    result
-}
-
-fn phase(input: &[isize], pattern: &[isize]) -> Result<Vec<isize>> {
-    let mut result = vec![];
-
-    for (index, _) in input.iter().enumerate() {
-        let mut sum = 0;
-        let pat: Vec<isize> = expand_pattern(index + 1, pattern)
-            .iter()
-            .cloned()
-            .cycle()
-            .take(input.len() + 1)
-            .skip(1)
-            .collect();
-        for (index, val) in input.iter().enumerate() {
-            sum += pat[index] * val;
-        }
-        let digit = sum
-            .to_string()
-            .chars()
-            .last()
-            .unwrap()
-            .to_digit(10)
-            .unwrap();
-        result.push(digit as isize);
-    }
+fn phase_parallel(input: &[isize], pattern: &[isize]) -> Result<Vec<isize>> {
+    let result = input
+        .par_iter()
+        .enumerate()
+        .map(|(i, _)| {
+            let sum: isize = input
+                .iter()
+                .enumerate()
+                .map(|(j, n)| pattern[((j + 1) / (i + 1) % 4) as usize] * n)
+                .sum();
+            (sum % 10).abs()
+        })
+        .collect();
 
     Ok(result)
 }
@@ -57,7 +37,7 @@ fn solve1(input: &[isize], pattern: &[isize], phases: usize) -> Result<String> {
     let mut input: Vec<isize> = input.iter().cloned().collect();
 
     for _phase in 0..phases {
-        input = phase(&input, pattern)?;
+        input = phase_parallel(&input, pattern)?;
     }
     let mut result = "".to_string();
 
@@ -69,11 +49,41 @@ fn solve1(input: &[isize], pattern: &[isize], phases: usize) -> Result<String> {
     Ok(result)
 }
 
+fn solve2(input: &[isize]) -> Result<String> {
+    slet input: Vec<isize> = input
+        .iter()
+        .cycle()
+        .take(input.len() * 10000)
+        .cloned()
+        .collect();
+
+    let s: String = input[..7].iter().map(|n| n.to_string()).collect();
+    let offset = s.parse::<usize>().unwrap();
+    let mut output: Vec<isize> = input[offset..].iter().cloned().collect();
+
+    for _ in 0..100 {
+        let mut sum = 0;
+        for i in 1..=output.len() {
+            let i = output.len() - i;
+            sum += output[i];
+            output[i] = sum % 10;
+        }
+    }
+
+    let mut result = "".to_string();
+    output
+        .iter()
+        .take(8)
+        .for_each(|i| result.push_str(&i.to_string()));
+
+    Ok(result)
+}
+
 fn main() -> Result<()> {
     let input = parse_input("resources/day16-input.txt")?;
     let pattern = [0, 1, 0, -1];
-
     println!("part 1: {}", solve1(&input, &pattern, 100)?);
+    println!("part 2: {}", solve2(&input)?);
     return Ok(());
 }
 
@@ -82,27 +92,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn expand_pattern_test() {
-        let pat = [0, 1, 0, -1];
-        let expanded_pattern = expand_pattern(3, &pat);
-
-        assert_eq!(12, expand_pattern(3, &pat).len());
-        for (k, v) in [0, 0, 0, 1, 1, 1, 0, 0, 0, -1, -1, -1].iter().enumerate() {
-            assert_eq!(*v, expanded_pattern[k]);
-        }
-    }
-
-    #[test]
     fn phase_test() {
         let input = [1isize, 2, 3, 4, 5, 6, 7, 8];
         let pattern = [0, 1, 0, -1];
         assert_eq!(
             &[4, 8, 2, 2, 6, 1, 5, 8].to_vec(),
-            &phase(&input, &pattern).unwrap()
+            &phase_parallel(&input, &pattern).unwrap()
         );
         assert_eq!(
             &[3, 4, 0, 4, 0, 4, 3, 8].to_vec(),
-            &phase(&[4, 8, 2, 2, 6, 1, 5, 8], &pattern).unwrap()
+            &phase_parallel(&[4, 8, 2, 2, 6, 1, 5, 8], &pattern).unwrap()
         );
     }
 
